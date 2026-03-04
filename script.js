ba4bc5307f331f7ebd64266bb6164e708506c73f
gustavomeco <82248764+gustavomeco@users.noreply.github.com>
2026-03-04 20:32:17 -0300

    feat: create operators functions and a function that call the operators

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;   
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+function operate(a,b,operator) {
+    switch(operator) {
+        case `+`:
+            return add(a,b);    
+        case `-`:
+            return subtract(a,b);
+        case `x`:
+            return multiply(a,b);
+        case `/`:
+            return divide(a,b);            
+    }
+
+};
+
+
+let n1,n2,operator;
